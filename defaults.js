b50a2dbb14df462a2c58e91e544b6c4e4c5e161d
Hunter Loftis <hunter@hunterloftis.com>
2014-07-21 11:34:06 -0700

    added debug

--- defaults.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  redis_url: 'redis://localhost:6379/0',
-  mongo_url: 'mongodb://localhost:27017/appTest',
-  port: 5000,
-  session_secret: 'mySecret'
-};
--- index.js
+++ index.js
@@ -1,59 +1,9 @@
-var express = require('express');
-var expressSession = require('express-session');
-var RedisStore = require('connect-redis')(expressSession);
-var mongoose = require('mongoose');
+var config = require('./lib/config');
+var app = require('./lib/app')(config);
+var debug = require('./lib/debug')('index');
 
-var defaults = require('./defaults');
-
-var REDIS_URL = process.env.REDISTOGO_URL || defaults.redis_url;
-var MONGO_URL = process.env.MONGOHQ_URL || defaults.mongo_url;
-var PORT = process.env.PORT || defaults.port;
-var SECRET = process.env.SESSION_SECRET || defaults.session_secret;
-
-var app = express();
-
-var store = new RedisStore({
-  url: REDIS_URL
-});
-
-var session = expressSession({
-  secret: SECRET,
-  store: store,
-  resave: true,
-  saveUninitialized: true
-});
-
-var db = mongoose.createConnection(MONGO_URL);
-
-db.on('connected', function() {
-  console.log('connected to mongo');
-});
-
-store.client.on('connect', function() {
-  console.log('connected to redis');
-});
-
-app
-  .use(session)
-  .get('/', fetchEntries, showEntries)
-  .post('/entries', createEntry, redirectHome);
-
-app.listen(PORT, function() {
-  console.log('listening on *:' + PORT);
-});
-
-function fetchEntries(req, res, next) {
-
-}
-
-function showEntries(req, res, next) {
-
-}
-
-function createEntry(req, res, next) {
-
-}
-
-function redirectHome(req, res, next) {
-
+if (!module.parent) {
+  app.listen(config.port, function() {
+    debug('listening on *:' + config.port);
+  });
 }
