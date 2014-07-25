cc0d7cfef8528ecbf0809f53ea0c24c95d1ce098
Hunter Loftis <hunter@hunterloftis.com>
2014-07-25 14:32:53 -0700

    basic functionality

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  app_name: 'Node-Production',
   redis_url: process.env.REDISCLOUD_URL || 'redis://localhost:6379/0',
   mongo_url: process.env.MONGOHQ_URL || 'mongodb://localhost:27017/appDev',
-  session_secret: process.env.SESSION_SECRET || 'mySecret',
+  session_secret: process.env.SESSION_SECRET || 'mySessionSecret',
+  cookie_secret: process.env.COOKIE_SECRET || 'myCookieSecret',
   show_errors: process.env.NODE_ENV !== 'production',
   port: process.env.PORT || 5000
 };
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var config = require('./lib/config');
+var http = require('http');
+var config = require('./config');
 var app = require('./lib/app')(config);
 var debug = require('./lib/debug')('index');
+var server;
 
 if (!module.parent) {
-  app.listen(config.port, function() {
-    debug('listening on *:' + config.port);
+  server = http.createServer(app);
+
+  server.listen(config.port, function() {
+    debug('listening on *:' + server.address().port);
   });
 }
