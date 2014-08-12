7f066eb7cfc4ff75cd100707d15020b34e437f07
Hunter Loftis <hunter@hunterloftis.com>
2014-08-12 14:03:25 -0700

    clustering

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
+var cluster = require('cluster');
+var cpus = require('os').cpus().length;
 var http = require('http');
 var config = require('./config');
 var app = require('./lib/app')(config);
 var debug = require('./lib/debug')('index');
-var server;
 
 if (!module.parent) {
-  server = http.createServer(app);
+  if (cluster.isMaster) master();
+  else worker();
+}
+
+function master() {
+  cluster.on('exit', revive);
+
+  for(var i = 0; i < cpus; i++) {
+    create(i, cpus);
+  }
+
+  function create(i, total) {
+    debug('Forking worker %d / %d ...', i, total);
+    cluster.fork();
+  }
+
+  function revive(worker, code, signal) {
+    console.error('Worker %d died (%s), restarting...', worker.process.pid, signal || code)
+    cluster.fork();
+  }
+}
+
+function worker() {
+  var server = http.createServer(app);
 
   server.listen(config.port, function() {
     debug('listening on *:' + server.address().port);
   });
 }
+
